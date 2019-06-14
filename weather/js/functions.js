9ca8e1665313098daa2d119e92c080f6b895ccd8
Night2013 <colton.l.craig@gmail.com>
2019-06-14 12:29:40 -0600

    putting wc var in

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -1,3 +1,12 @@
 // Weather Site JavaScript Functions
 
 console.log('My javascript is being read.');
+
+// Calculate the Windchill
+function buildWC(speed, temp) {
+    // Compute the windchill
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
+
+    const feelTemp = document.getElementById('feelTemp');
+}
