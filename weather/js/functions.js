31559d077424becd8b0d9c2c743422310bdd5a75
Night2013 <colton.l.craig@gmail.com>
2019-06-15 19:44:19 -0600

    built wind chill function

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -2,10 +2,15 @@
 
 console.log('test');
 console.log('My javascript is being read.');
+
+// Variables for Function Use
+const temp = 31;
+const speed = 5;
 buildWC(speed, temp);
 
 // Calculate the Windchill
 function buildWC(speed, temp) {
+  const feelTemp = document.getElementById('feelTemp');   
     // Compute the windchill
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log(wc);
@@ -16,5 +21,7 @@ function buildWC(speed, temp) {
     // If chill is greater than temp, return the temp
     wc = (wc > temp)?temp:wc;
 
-    const feelTemp = document.getElementById('feelTemp');
+    // Display the windchill
+    console.log(wc);
+    feelTemp.innerHTML = wc;
 }
