d0a63a567c46e7cc3646ceab3711af3a63c1c40e
Night2013 <colton.l.craig@gmail.com>
2019-07-17 12:55:09 -0600

    post review

--- acme-project/js/functions.js
+++ acme-project/js/functions.js
@@ -1,21 +1,83 @@
 'use strict';
 
-let navListItems = "/acme/js/acme.json";
 
-console.log("test");
-fetchData(navListItems);
 
-console.log("test2");
-function fetchData(navListItems) {
+let navBar = document.getElementById('nav-bar');
+
+navBar.addEventListener('click', function (evt) {
+
+    let pageName = evt.target.innerHTML;
+    switch (pageName) {
+        case 'Anvils':
+        case 'Explosives':
+        case 'Decoys':
+        case 'Traps':
+            evt.preventDefault();
+            break;
+    }
+
+    let acmeURL = '/acme-project/js/acme.json';
     
-    fetch(navListItems)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('Network response was not O')
-        })
-        .then(function (data) {
-            console.log(data.navList);
-        })
-}
+    fetch(acmeURL)
+    .then(function(response) {
+    if(response.ok){
+    return response.json();
+    }
+    throw new ERROR('Network response was not OK.');
+    })
+    .then(function(data){
+      // Check the data object that was retrieved
+      console.log(data);
+
+        // Give a shortcut
+        let info = data[pageName];
+        
+        // Create variable and put them into object
+
+        let description = info.description;
+        // Check
+        console.log(description);
+
+        let name = info.name;
+        // Check
+        console.log(name);
+
+        let path = info.path;
+        // Check
+        console.log(path);
+
+        let manufacturer = info.manufacturer;
+        // Check
+        console.log(manufacturer);
+
+        let price = info.price;
+        // Check
+        console.log(price);
+
+        let reviews = info.reviews;
+        // Check
+        console.log(reviews);
+
+        // Start putting info in page
+        document.getElementById('name').innerHTML = name;
+        document.getElementById('description').innerHTML = description;
+        document.getElementById('path').src = path;
+        document.getElementById('manufacturer').innerHTML = manufacturer;
+        document.getElementById('contentReviews').innerHTML = reviews;
+        document.getElementById('price').innerHTML = 'Price: ' + price;
+
+        // Change title page
+        document.getElementById('titlePage').innerHTML = 'ACME | ' + name;
+
+        // Display content and hide home page
+        contentPage.setAttribute('class', '');
+        homePage.setAttribute('class', 'hide');
+    })
+    .catch(function(error){
+    console.log('There was a fetch problem: ', error.message);
+
+
+    statusContainer.innerHTML = 'Sorry, the data could not be processed.';
+    })
+  
+  })
